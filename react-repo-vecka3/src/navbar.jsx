695862ba6db40bf37826a5712d44c65e7d53a52d
Asplund-chasacademy <andreas.asplund@chasacademy.se>
2025-01-16 15:43:44 +0100

    detta är fram till övning 10 på net ninja

---
+++ react-repo-vecka3/src/navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+        <h1> Te dojo blog</h1>
+        <div className="links">
+            <a href="/"> home</a>
+            <a href="/create"> New blog</a>
+        </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
